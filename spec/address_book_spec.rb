6162112b79a4c964995ca2a8726ddd9d575606f2
Grant <gsbackes@gmail.com>
2017-08-22 04:28:09 +0900

    Assignment 6: completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -69,9 +69,8 @@ RSpec.describe AddressBook do
       book.entries.each do |entry|
 
         expect(entry.name).not_to eq('Ada Lovelace')  
-
       end
-    end
+    end  
   end
 
   # Test that AddressBook's .import_from_csv() method is working as expected
@@ -114,5 +113,30 @@ RSpec.describe AddressBook do
       entry_five = book.entries[4]
       check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
     end
+
+    it "imports the correct number of entries from 2nd csv file" do
+      book.import_from_csv("models/entries2.csv")
+      book_size = book.entries.size
+      expect(book_size).to eq 3
+    end
+
+    it "imports the 1st entry from 2nd csv file" do
+      book.import_from_csv("models/entries2.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end 
+
+    it "imports the 2nd entry from 2nd csv file" do
+      book.import_from_csv("models/entries2.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+    
+    it "imports the 3rd entry from 2nd csv file" do
+      book.import_from_csv("models/entries2.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+    end
+
   end
 end
